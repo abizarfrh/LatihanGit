172e77619ae1fb0f2fc8556ab8a662d388c63a5f
Abizar Faroughi <abizarfrgh@gmail.com>
2024-08-27 08:55:23 +0700

    Menambahkan folder Tugas 10

---
+++ Tugas 10/contact.js
@@ -0,0 +1,17 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    if (name === '' || email === '' || message === '') {
+        alert('Semua field harus diisi!');
+        event.preventDefault();
+        return;
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        alert('Email tidak valid!');
+        event.preventDefault();
+    }
+});
